ec58b45b8707c3735b806f58ffab26238db1f0c3
Robin-Majumdar <robinmajumdarofficial@gmail.com>
2026-03-11 22:36:01 +0600

    feat: add cash out transaction history with live balance update

--- script/cash-out.js
+++ script/cash-out.js
@@ -38,6 +38,27 @@ document.getElementById('cash-out-button')
         if (cashOutPin === 1234) {
             const newBalance = accountBalance - cashOutMoney;
             document.getElementById('account-balance').innerText = newBalance;
+
+            const now = new Date();
+            const date = now.toLocaleDateString();
+            const time = now.toLocaleTimeString();
+
+            const transactionHistory = document.getElementById('history');
+            const newTransaction = document.createElement('div');
+
+            newTransaction.innerHTML = `
+                    <div class="bg-gray-200 flex items-center p-4 gap-4 rounded-xl">
+
+                    <div class="bg-[#F4F5F7] p-2 rounded-full">
+                        <img src="./assets/icon/cash-out.png" alt="Cash out icon">
+                    </div>
+                    <div>
+                        <p class=''> ${cashOutMoney} Tk cash out successfully, from ${agentNumber}. Total balance: ${newBalance} Tk.</p>
+                        <p class='text-sm'> On ${date} at ${time}</p>
+                    </div>
+                </div>`;
+
+            transactionHistory.append(newTransaction);
         }
         else {
             alert('Cash out failed. Please try again!');
